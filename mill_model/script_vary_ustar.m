97ddf00404afec8218f305e1539b2021bf9b5b12
amoodie <amoodie@rice.edu>
2018-06-25 11:32:09 -0500

    add documentation and initialize the varyustar case

--- mill_model/script_vary_ustar.m
+++ mill_model/script_vary_ustar.m
@@ -13,7 +13,7 @@ gs.Properties.VariableNames = {'class', 'perc'};
 mill.D = []; % assigned in loop, sediment grain size in m
 mill.H = 0.5; % flow depth in m
 mill.kc = 2e-3; % composite roughness height in m (including effect of bedforms if present)
-mill.ustar = 0.05; % shear velocity in m/s
+mill.ustar = []; % assigned in loop, shear velocity in m/s
 mill.zetar = 0.05; % reference height for beginning zeta vector
 mill.nzeta = 50; % number of points in zeta
 mill.zeta = linspace(mill.zetar, 1, mill.nzeta+1)';
@@ -39,25 +39,28 @@ opts.Cb = 1e-3;
 
 
 %% compute the concentration profile for each grain class
-for i = 1:size(gs, 1)
-    % select appropriate params
-    mill.D = gs.class(i) * 1e-6;
+for i = 1:length(ustar_range)
+    %% select the loop ustar
+    mill.ustar = ustar_range(i);
     
-    % calculate the denstity strat profile
-    [un1gsDS, cn1gsDS, u1gsDS, c1gsDS, ~] = denstrat_1class(mill, soln, opts, con);
-    uDS(:, i) = u1gsDS .* (gs.perc(i) / 100);
-    cDS(:, i) = c1gsDS .* (gs.perc(i) / 100);
+    for j = 1:size(gs, 1)
+        mill.D = gs.class(j) * 1e-6;
+        
+        % calculate the denstity strat profile
+        [un1gsDS, cn1gsDS, u1gsDS, c1gsDS, ~] = denstrat_1class(mill, soln, opts, con);
+        uDS(:, j, i) = u1gsDS .* (gs.perc(j) / 100);
+        cDS(:, j, i) = c1gsDS .* (gs.perc(j) / 100);
+        
+        % calculate the rouse profile
+        [cn1gsRou, c1gsRou] = rouse_1class(mill, opts, con);
+        cRou(:, j, i) = c1gsRou .* (gs.perc(j) / 100);
+    end
     
-    % calculate the rouse profile
-    [cn1gsRou, c1gsRou] = rouse_1class(mill, opts, con);
-    cRou(:, i) = c1gsRou .* (gs.perc(i) / 100);
+    %% sum the grain classes into one profile
+    uSumDS = sum(uDS, 2);
+    cSumDS = sum(cDS, 2);
+    cSumRou = sum(cRou, 2);
 end
-
-%% sum the grain classes into one profile
-uSumDS = sum(uDS, 2);
-cSumDS = sum(cDS, 2);
-cSumRou = sum(cRou, 2);
-
 
 %% make some plots
 figure(); hold on;
--- mill_model_docs.md
+++ mill_model_docs.md
@@ -1,21 +1,15 @@
-
-
-* `script_1paramset.m` 
-
-
-
 ## Scripts
 Scripts written to manipulate these functions (i.e., to make a model) should be prefixed with `script_`.
 
-### `script_1paramset.m`
+#### `script_1paramset.m`
 Is the simplest case where one grain size _distribution_ is suspended into a mill with one parameter set.
 Calculated for both the DS and Rouse case and compared.
 
 
-### `script_simple_suspension.m`
+#### `script_simple_suspension.m`
 This is a simple Rouse model.
 
-### `script_stratify.m`
+#### `script_stratify.m`
 This is a direct reimplementation of the parker ebook RTe-bookSuspSedDensityStrat in Matlab.
 This code solves for the DS adjusted profile for one grain class.
 The profiles are plotted at the end of the calculation (against the "no stratificaion" case).
@@ -24,27 +18,27 @@ The profiles are plotted at the end of the calculation (against the "no stratifi
 ## Functions
 Functions may have any namespace, but try to make it descriptive and self-similar for functions serving a similar purpose.
 
-### `denstrat_1class.m`
+#### `denstrat_1class.m`
 
 
-### `entr_WP04.m`
+#### `entr_WP04.m`
 
 
-### `find_Dxx.m`
+#### `find_Dxx.m`
 
 
-### `load_grainsize.m`
+#### `load_grainsize.m`
 
 
-### `makeDistToData.m`
+#### `makeDistToData.m`
 
 
-### `normalize_model.m`
+#### `normalize_model.m`
 
 
-### `rouse_1class.m`
+#### `rouse_1class.m`
 
 
-### `rouse.m`
+#### `rouse.m`
 
 
